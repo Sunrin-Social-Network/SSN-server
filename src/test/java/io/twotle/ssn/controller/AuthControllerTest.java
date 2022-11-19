4cc3a0cb289b33d2e388eea3f05ba5d38f6f84f6
2tle <iam@2tle.io>
2022-11-20 07:26:37 +0900

    IMPORTANT: build.gradle

--- src/test/java/io/twotle/ssn/controller/AuthControllerTest.java
+++ src/test/java/io/twotle/ssn/controller/AuthControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Rollback;
